174c154f4daf0d1475fd50745294b364207b6aab
sapir054 <xphrsd@gmail.com>
2021-06-22 15:03:13 +0300

    login navbar

--- client/src/components/loginForm/Login.tsx
+++ client/src/components/loginForm/Login.tsx
@@ -1,18 +1,15 @@
-import React,{useState} from 'react'
-import { Form, Input, Button, Select } from 'antd';
-import { connect } from 'react-redux';
-import usersActions from '../../redux/actions/user.actions';
-import LogIn from '../../utils/login.utils';
-const { Option } = Select;
+import React, { useState } from 'react'
+import { Form, Input, Button, Select } from 'antd'
+import { connect } from 'react-redux'
+import usersActions from '../../redux/actions/user.actions'
+import LogIn from '../../utils/login.utils'
+const { Option } = Select
 
 const { loginUser, setTokenLocal } = LogIn
-const { setUserData } = usersActions.usersActions;
+const { setUserData } = usersActions.usersActions
 
-const mapDispatchToProps = (dispatch: any) => ({setUserData: (data: Object) => { dispatch(setUserData(data)) }})
-const mapStateToProps = (state: any) => { return { userData: state.user.userData } }
-
-const Login = (props:any): any => {
-    const{isLogIn,showModal}=props
+const Login = (props: any): any => {
+    const { isLogIn, showModal, setUserData } = props
     const [role, setRole] = useState('student')
     const layout = { labelCol: { span: 8 }, wrapperCol: { span: 16 } }
     const tailLayout = { wrapperCol: { offset: 8, span: 16 } }
@@ -21,23 +18,28 @@ const Login = (props:any): any => {
         const email: any = values.email
         const password: any = values.password
         const resFromLogin = await loginUser({ email, password }, role)
-        console.log(resFromLogin);
-        console.log(email);
-        console.log(password);
-        
+        console.log(resFromLogin)
+        console.log(email)
+        console.log(password)
+
         if (resFromLogin.success) {
-            isLogIn(true)
+            // isLogIn(true)
             const token = resFromLogin.token
+            console.log('in success')
+            console.log(token)
             setTokenLocal(token)
             setUserData(token)
+
             // window.location.reload();
             showModal()
-        } else { alert('נסה שוב')} 
-          
-
+        } else {
+            alert('נסה שוב')
+        }
     }
-    const onFinishFailed = (errorInfo: any) => { console.log('Failed:', errorInfo) }
-    const handleChange = (value: any) =>{
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo)
+    }
+    const handleChange = (value: any) => {
         setRole(value)
     }
     return (
@@ -64,21 +66,34 @@ const Login = (props:any): any => {
                 <Input.Password />
             </Form.Item>
 
-
-            <div style={{ display: 'flex', justifyContent: 'space-between', }}>
-                <Form.Item  {...tailLayout} >
-                    <Button  type="primary" htmlType="submit"> התחבר </Button>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                        {' '}
+                        התחבר{' '}
+                    </Button>
                 </Form.Item>
-                <Select defaultValue="student" style={{ width: 120, height: '110%' }} onChange={handleChange}>
+                <Select
+                    defaultValue="student"
+                    style={{ width: 120, height: '110%' }}
+                    onChange={handleChange}
+                >
                     <Option value="student">Student</Option>
                     <Option value="hr">Hr</Option>
                     <Option value="admin">Admin</Option>
                 </Select>
             </div>
         </Form>
-    );
-};
+    )
+}
 
-
+const mapDispatchToProps = (dispatch: any) => ({
+    setUserData: (data: Object) => {
+        dispatch(setUserData(data))
+    },
+})
+const mapStateToProps = (state: any) => {
+    return { userData: state.user.userData }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login)
--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -1,118 +1,141 @@
-import React, { useState,useEffect } from "react";
-import "./NavBar.css";
-import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { MdFingerprint } from "react-icons/md";
-import Button from "../Button"
-import { Layout, Menu, Modal } from "antd";
-import Login from "../loginForm/Login";
+import React, { useState, useEffect } from 'react'
+import './NavBar.css'
+import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { MdFingerprint } from 'react-icons/md'
+import Button from '../Button'
+import { Layout, Menu, Modal } from 'antd'
+import Login from '../loginForm/Login'
 
-import {IconContext} from 'react-icons/lib'
+import { IconContext } from 'react-icons/lib'
+import loginUtils from '../../utils/login.utils'
+import { useSelector } from 'react-redux'
+
+const isObjectEmpty = (obj: any) => Object.keys(obj).length === 0
 
 function NavBar() {
-  const [btnTxtCng, setBtnTxtCng] = useState(false);
-  const [click, setClick] = useState(false);
-  const [isButton,setIsButton] = useState(true)
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const handleClick = () => setClick(!click);
+    const isLogged = useSelector((state: any) => {
+        console.log(state)
+        return !isObjectEmpty(state.user)
+    })
 
-const closeMobileMenu=() => setClick(false)
-const showButton = () =>{
-  if (window.innerWidth<=960){
-    setIsButton(false)
-  }else {
-    setIsButton(true)
-  }
-};
-const showModal = () => {
-  setIsModalVisible(state=>!state);
-};
-const handleCancel = () => {
-  setIsModalVisible(false);
-};
-useEffect(()=>{
-  showButton()
-},[])
-window.addEventListener("resize",showButton);
-  return (
-    <>
-    <IconContext.Provider value={{color:'#000'}}>
-      <div className="navbar">
-        <div className="navbar-container container">
-          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-          <img className="logosmall" src="/connectech-color-smaller.png" />
-            ConnecTech
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            {click ? <FaTimes /> : <FaBars />}
-          </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links">
-                עמוד הבית
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/student" className="nav-links" onClick={closeMobileMenu}>
-                סטודנטים
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/hr" className="nav-links" onClick={closeMobileMenu}>
-                מגייסים
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/recruiter" className="nav-links" onClick={closeMobileMenu}>
-              משרות
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/admin" className="nav-links" onClick={closeMobileMenu}>
-                הנהלה
-              </Link>
-            </li>
-            <li className="nav-btn">
-           
-             
-                {/* {isButton? (
-            <Button size="large" type="primary" onClick={btnTxtCng}>
-              {" התחבר"}
-            </Button>
-          ) : (
-            <Button size="large" type="primary" onClick={showModal}>
-              {" התנתק"}
-            </Button>
-          )} */}
-          
- {isButton ? (<div>
-                <Link to="/signup" className="btn-link" > 
-                  <Button onClick={showModal} buttonStyle='btn--outline'>{btnTxtCng?"התנתק":"התחבר"}</Button>
-                 </Link>
+    console.log(isLogged)
+    const [btnTxtCng, setBtnTxtCng] = useState(false)
+    const [click, setClick] = useState(false)
+    const [isButton, setIsButton] = useState(true)
+    const [isModalVisible, setIsModalVisible] = useState(false)
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setIsButton(false)
+        } else {
+            setIsButton(true)
+        }
+    }
+    const showModal = () => {
+        setIsModalVisible((state) => !state)
+    }
+    const handleCancel = () => {
+        setIsModalVisible(false)
+    }
+    const logOutHandler = () => {
+        loginUtils.removeToken()
+        window.location.reload()
+    }
+    useEffect(() => {
+        showButton()
+    }, [])
+    window.addEventListener('resize', showButton)
 
-                <Modal
-            title="התחברות"
-            visible={isModalVisible}
-            onCancel={handleCancel}
-            footer={null}
-          >
-            <Login isLogIn={setBtnTxtCng} showModal={showModal}/>
-          </Modal>
+    return (
+        <>
+            <IconContext.Provider value={{ color: '#000' }}>
+                <div className="navbar">
+                    <div className="navbar-container container">
+                        <Link
+                            to="/"
+                            className="navbar-logo"
+                            onClick={closeMobileMenu}
+                        >
+                            <img
+                                className="logosmall"
+                                src="/connectech-color-smaller.png"
+                            />
+                            ConnecTech
+                        </Link>
+                        <div className="menu-icon" onClick={handleClick}>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </div>
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                            <li className="nav-item">
+                                <Link to="/" className="nav-links">
+                                    עמוד הבית
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    to="/student"
+                                    className="nav-links"
+                                    onClick={closeMobileMenu}
+                                >
+                                    סטודנטים
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    to="/hr"
+                                    className="nav-links"
+                                    onClick={closeMobileMenu}
+                                >
+                                    מגייסים
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    to="/recruiter"
+                                    className="nav-links"
+                                    onClick={closeMobileMenu}
+                                >
+                                    משרות
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    to="/admin"
+                                    className="nav-links"
+                                    onClick={closeMobileMenu}
+                                >
+                                    הנהלה
+                                </Link>
+                            </li>
+                            <li className="nav-btn">
+                                <Button
+                                    onClick={
+                                        isLogged ? logOutHandler : showModal
+                                    }
+                                    buttonStyle="btn--outline"
+                                >
+                                    {isLogged ? 'log out' : 'log in'}
+                                </Button>
+                                <Modal
+                                    title="התחברות"
+                                    visible={isModalVisible}
+                                    onCancel={handleCancel}
+                                    footer={null}
+                                >
+                                    <Login
+                                        isLogIn={setBtnTxtCng}
+                                        showModal={showModal}
+                                    />
+                                </Modal>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-              ): (
-                <Link to="/signup" className="btn-link" onClick={closeMobileMenu}>
-                  <Button buttonStyle='btn--outline' buttonSize='btn--mobile'>התחבר</Button>
-                 
-                  
-                  
-                </Link> )}
-            </li>
-          </ul>
-        </div>
-      </div>
-      </IconContext.Provider>
-    </>
-  );
+            </IconContext.Provider>
+        </>
+    )
 }
 
-export default NavBar;
+export default NavBar
